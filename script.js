36c008eabce9b4eb6e56c3180ac212ad67083036
DanyMencu <dany69men@gmail.com>
2021-11-16 10:32:09 +0100

    Project setup upload

---
+++ script.js
@@ -0,0 +1,8 @@
+//* Milestone 1
+//Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+
+const root = new Vue ({
+    el: '#root',
+    data: {},
+    methods: {},
+}),
